0b3d653bd18cd3ec8a6599cf53f9f3a7837d89a7
Matheusrst <143093822+Matheusrst@users.noreply.github.com>
2024-06-01 10:03:51 -0300

    atualizando

--- source/Models/cafeApp/appWallet.php
+++ source/Models/cafeApp/appWallet.php
@@ -2,10 +2,23 @@
 
 namespace Source\Models\cafeApp;
 
+use Source\Core\Model;
+use Source\Models\User;
+
 class appWallet extends Model
 {
     public function __construct()
     {
         parent::__construct("app_wallets", ["id"], ["user_id", "wallwt"]);
+    }
+
+    public function start(user $user): AppWallet
+    {
+        if (!$this->find("user_id = :user", "user={$user->id}")->count()) {
+            $this->user_id = $user->id;
+            $this->wallet = "Minha Carteira";
+            $this->save();
+        }
+        return $this;
     }
 }
